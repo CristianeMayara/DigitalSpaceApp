8b64c653a84a0aaf8026e2ef0d90532dcc9420de
Cristiane Mayara <cris.mayara@gmail.com>
2018-06-29 09:45:35 -0300

    Adding Favorite Fragment

--- app/src/main/java/com/cristiane/joyjetapp/Fragments/FavoriteListFragment.java
+++ app/src/main/java/com/cristiane/joyjetapp/Fragments/FavoriteListFragment.java
@@ -11,38 +11,26 @@ import android.view.ViewGroup;
 
 import com.cristiane.joyjetapp.Activities.BaseActivity;
 import com.cristiane.joyjetapp.Adapters.CategoryListAdapter;
+import com.cristiane.joyjetapp.Adapters.FavoriteListAdapter;
 import com.cristiane.joyjetapp.R;
 
 /**
- * Created by cristiane on 28/06/2018.
+ * Created by cristiane on 29/06/2018.
  */
 
-public class CategoryListFragment extends Fragment {
-
+public class FavoriteListFragment extends Fragment {
     public static final String TAG = "CategoryListFragment";
-    public static final String ARG_ARTICLES = "articles";
-    private RecyclerView rvArticleList;
+    private RecyclerView rvFavoriteList;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-    //private List<ArticleTypeItem> articleItems;
 
-    public static CategoryListFragment newInstance() {
-        return new CategoryListFragment();
-        /*CategoryListFragment fragment = new CategoryListFragment();
-        Bundle bundle = new Bundle();
-        bundle.putParcelableArrayList(ARG_ARTICLES, (ArrayList<? extends Parcelable>) articleItems);
-        fragment.setArguments(bundle);
-        return fragment;*/
+    public static FavoriteListFragment newInstance() {
+        return new FavoriteListFragment();
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*if(getArguments() != null) {
-            articleItems = getArguments().getParcelableArrayList(ARG_ARTICLES);
-        } else if(savedInstanceState != null) {
-            articleItems = savedInstanceState.getParcelableArrayList(ARG_ARTICLES);
-        }*/
     }
 
     @Nullable
@@ -50,22 +38,21 @@ public class CategoryListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_category_list, container, false);
         initComponents(rootView);
-
         return rootView;
     }
 
     private void initComponents(View rootView) {
         layoutManager = new LinearLayoutManager(getContext());
-        rvArticleList = rootView.findViewById(R.id.rv_category_list);
-        rvArticleList.setLayoutManager(layoutManager);
-        rvArticleList.setHasFixedSize(true);
+        rvFavoriteList = rootView.findViewById(R.id.rv_category_list);
+        rvFavoriteList.setLayoutManager(layoutManager);
+        rvFavoriteList.setHasFixedSize(true);
 
         updateAdapter();
     }
 
     private void updateAdapter() {
-        CategoryListAdapter adapter = new CategoryListAdapter(getContext(), BaseActivity.getData());
-        rvArticleList.setAdapter(adapter);
+        FavoriteListAdapter adapter = new FavoriteListAdapter(getContext(), BaseActivity.getFavorites());
+        rvFavoriteList.setAdapter(adapter);
         adapter.notifyDataSetChanged();
     }
 }
