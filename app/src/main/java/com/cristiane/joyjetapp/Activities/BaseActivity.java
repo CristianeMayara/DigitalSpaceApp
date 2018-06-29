782d1bce27fc4ee5893d0c3fd012a1d764ebdbca
Cristiane Mayara <cris.mayara@gmail.com>
2018-06-28 23:02:34 -0300

    Using base activity and fragments

--- app/src/main/java/com/cristiane/joyjetapp/Activities/BaseActivity.java
+++ app/src/main/java/com/cristiane/joyjetapp/Activities/BaseActivity.java
@@ -1,8 +1,8 @@
 package com.cristiane.joyjetapp.Activities;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,20 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import com.cristiane.joyjetapp.Adapters.CategoryListAdapter;
+import com.cristiane.joyjetapp.Fragments.CategoryListFragment;
 import com.cristiane.joyjetapp.Model.Article;
 import com.cristiane.joyjetapp.Model.ArticleTypeItem;
 import com.cristiane.joyjetapp.R;
 
 import java.util.ArrayList;
 
-public class ArticleListActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    public static final String TAG = "ArticleListActivity";
-    private RecyclerView rvArticleList;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    public static final String TAG = "BaseActivity";
 
     private Toolbar toolbar;
     DrawerLayout drawer;
@@ -32,22 +28,20 @@ public class ArticleListActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_article_list);
+        setContentView(R.layout.activity_base);
 
         initComponents();
+
+        Fragment fragment = CategoryListFragment.newInstance();
+        commitFragment(fragment, CategoryListFragment.TAG);
+        getSupportActionBar().setTitle(getString(R.string.category_list_screen_title));
     }
 
     private void initComponents() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        layoutManager = new LinearLayoutManager(this);
-        rvArticleList = findViewById(R.id.rv_article_list);
-        rvArticleList.setLayoutManager(layoutManager);
-        rvArticleList.setHasFixedSize(true);
-
         setDrawerMenu();
-        updateAdapter();
     }
 
     private void setDrawerMenu() {
@@ -61,13 +55,7 @@ public class ArticleListActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void updateAdapter() {
-        CategoryListAdapter adapter = new CategoryListAdapter(this, getData());
-        rvArticleList.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-    }
-
-    private ArrayList<ArticleTypeItem> getData() {
+    public static ArrayList<ArticleTypeItem> getData() {
         ArrayList<Article> articles = new ArrayList<>();
         articles.add(new Article(1, "International Space Station", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", "Lorem ipsum dolor sit amet, consectuer adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.", 1, false));
         articles.add(new Article(2, "My Capsule", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.", "Lorem ipsum dolor sit amet, consectuer adipiscing elit.', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.", 2, false));
@@ -99,6 +87,9 @@ public class ArticleListActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            Fragment fragment = CategoryListFragment.newInstance();
+            commitFragment(fragment, CategoryListFragment.TAG);
+            getSupportActionBar().setTitle(getString(R.string.category_list_screen_title));
 
         } else if (id == R.id.nav_gallery) {
 
@@ -114,5 +105,20 @@ public class ArticleListActivity extends AppCompatActivity
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void commitFragment(Fragment fragment, String tag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_content, fragment, tag)
+                .addToBackStack(tag)
+//                .setCustomAnimations(
+//                        R.anim.card_flip_right_in,
+//                        R.anim.card_flip_right_out,
+//                        R.anim.card_flip_left_in,
+//                        R.anim.card_flip_left_out)
+//                .setCustomAnimations(R.anim.res_anim_fadein, R.anim.res_anim_fadeout)
+                .commitAllowingStateLoss();
     }
 }
---
+++ app/src/main/java/com/cristiane/joyjetapp/Fragments/CategoryListFragment.java
@@ -0,0 +1,62 @@
+package com.cristiane.joyjetapp.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cristiane.joyjetapp.Activities.BaseActivity;
+import com.cristiane.joyjetapp.Adapters.CategoryListAdapter;
+import com.cristiane.joyjetapp.R;
+
+/**
+ * Created by cristiane on 28/06/2018.
+ */
+
+public class CategoryListFragment extends Fragment {
+
+    public static final String TAG = "CategoryListFragment";
+    private RecyclerView rvArticleList;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    public static CategoryListFragment newInstance() {
+        CategoryListFragment fragment = new CategoryListFragment();
+        Bundle bundle = new Bundle();
+//        bundle.putParcelable(ARG_USER, user);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_category_list, container, false);
+        initComponents(rootView);
+        return rootView;
+    }
+
+    private void initComponents(View rootView) {
+        layoutManager = new LinearLayoutManager(getContext());
+        rvArticleList = rootView.findViewById(R.id.rv_article_list);
+        rvArticleList.setLayoutManager(layoutManager);
+        rvArticleList.setHasFixedSize(true);
+
+        updateAdapter();
+    }
+
+    private void updateAdapter() {
+        CategoryListAdapter adapter = new CategoryListAdapter(getContext(), BaseActivity.getData());
+        rvArticleList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+}
